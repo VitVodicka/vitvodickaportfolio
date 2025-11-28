11726068ad383bf398bc220f52fe46fc2bb925d4
Vit Vodicka <xvodick6@mendelu.cz>
2025-11-28 17:04:34 +0100

    hobbies part done

--- frontend/vue-project/src/components/hobbies.vue
+++ frontend/vue-project/src/components/hobbies.vue
@@ -1,66 +1,90 @@
 <template>
-<h1>Mé dovednosti</h1>
 
-<v-container>
+<v-container class="pa-4">
+    <h1>Hobbies</h1>
     <v-row>
-        <v-col>
-<v-card color="itemsPrimary" class="border border-2">
-    Frontend
-  <v-card-text>
-    <ul>
-      <li>Programování v jazycích: JavaScript, Python, C++</li>
-      <li>Webový vývoj: HTML, CSS, Vue.js, React</li>
-      <li>Databáze: MySQL, MongoDB</li>
-      <li>Verzovací systémy: Git, GitHub</li>
-      <li>Jazykové dovednosti: Angličtina (pokročilá), Čeština (rodilý mluvčí)</li>
-    </ul>
-  </v-card-text>
-  </v-card>
-  </v-col>
 
-  <v-col>
-<v-card color="itemsPrimary" class="border border-2">
-    Frontend
-  <v-card-text>
-    <ul>
-      <li>Programování v jazycích: JavaScript, Python, C++</li>
-      <li>Webový vývoj: HTML, CSS, Vue.js, React</li>
-      <li>Databáze: MySQL, MongoDB</li>
-      <li>Verzovací systémy: Git, GitHub</li>
-      <li>Jazykové dovednosti: Angličtina (pokročilá), Čeština (rodilý mluvčí)</li>
-    </ul>
-  </v-card-text>
-  </v-card>
-  </v-col>
+<v-col>
+<v-card color="background" class="hobbiesCards" >
+    <v-card-title>Frontend</v-card-title>
+    <v-card-text>
+        <div style="display:flex; flex-wrap:wrap; gap:8px;">
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+
+        </div>
+    </v-card-text>
+</v-card>
+</v-col>
+
+<v-col>
+<v-card color="background" class="hobbiesCards" >
+    <v-card-title>Frontend</v-card-title>
+    <v-card-text>
+        <div style="display:flex; flex-wrap:wrap; gap:8px;">
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+
+        </div>
+    </v-card-text>
+</v-card>
+</v-col>
+
+<v-col>
+<v-card color="background" class="hobbiesCards" >
+    <v-card-title>Frontend</v-card-title>
+    <v-card-text>
+        <div style="display:flex; flex-wrap:wrap; gap:8px;">
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+
+        </div>
+    </v-card-text>
+</v-card>
+</v-col>
+
+<v-col>
+<v-card color="background" class="hobbiesCards" >
+    <v-card-title>Frontend</v-card-title>
+    <v-card-text>
+        <div style="display:flex; flex-wrap:wrap; gap:8px;">
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+            <v-chip style="flex" class="v-chip">ble</v-chip>
+
+        </div>
+    </v-card-text>
+</v-card>
+</v-col>
 
 
 
-    <v-col>
-<v-card color="itemsPrimary" class="border border-2">
-    Frontend
-  <v-card-text>
-    <ul>
-      <li>Programování v jazycích: JavaScript, Python, C++</li>
-      <li>Webový vývoj: HTML, CSS, Vue.js, React</li>
-      <li>Databáze: MySQL, MongoDB</li>
-      <li>Verzovací systémy: Git, GitHub</li>
-      <li>Jazykové dovednosti: Angličtina (pokročilá), Čeština (rodilý mluvčí)</li>
-    </ul>
-  </v-card-text>
-  </v-card>
-  </v-col>
-
-
-  
-
-
-  
-
-  
 
   </v-row>
   </v-container>
 </template>
+<style scoped>
+.hobbiesCards {
+    border: 2px solid  #374151;
+    border-radius: 5px;
+
+}
+.v-chip{
+    display: flex;
+    color: #137fec;
+    background-color: #101922;
+    padding: 5px;
+    margin: 5px;;
+    border-radius: 5px;
+}
+</style>
 <script>
 export default {
   name: 'Hobbies',
